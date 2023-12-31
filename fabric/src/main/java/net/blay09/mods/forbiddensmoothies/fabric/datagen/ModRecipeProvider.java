318f5123c7bd926f38799d21fc24405f77d204f6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-31 12:39:23 +0100

    feat: Add Ugly Steel Plating recipe

--- fabric/src/main/java/net/blay09/mods/forbiddensmoothies/fabric/datagen/ModRecipeProvider.java
+++ fabric/src/main/java/net/blay09/mods/forbiddensmoothies/fabric/datagen/ModRecipeProvider.java
@@ -1,14 +1,13 @@
 package net.blay09.mods.forbiddensmoothies.fabric.datagen;
 
 import net.blay09.mods.forbiddensmoothies.block.ModBlocks;
+import net.blay09.mods.forbiddensmoothies.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
 
 import java.util.function.Consumer;
 
@@ -39,6 +38,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .define('I', Items.IRON_INGOT)
                 .define('F', Items.FLINT)
                 .unlockedBy("has_flint", has(Items.FLINT))
+                .save(exporter);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, ModItems.uglySteelPlating)
+                .pattern(" I ")
+                .pattern("IPI")
+                .pattern(" I ")
+                .define('P', Items.POTATO)
+                .define('I', Items.IRON_INGOT)
+                .unlockedBy("has_potato", has(Items.POTATO))
                 .save(exporter);
     }
 }
