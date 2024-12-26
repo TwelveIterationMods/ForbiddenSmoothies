360280e33cb7a432125b715d53bcd6a7ea476fac
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-12-26 15:24:00 +0100

    chore: Some cleanup

--- shared/src/main/java/net/blay09/mods/forbiddensmoothies/ForbiddenSmoothies.java
+++ shared/src/main/java/net/blay09/mods/forbiddensmoothies/ForbiddenSmoothies.java
@@ -7,18 +7,10 @@ import net.blay09.mods.forbiddensmoothies.menu.ModMenus;
 import net.blay09.mods.forbiddensmoothies.item.ModItems;
 import net.blay09.mods.forbiddensmoothies.network.ModNetworking;
 import net.blay09.mods.forbiddensmoothies.block.entity.ModBlockEntities;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ForbiddenSmoothies {
 
-    // TODO better textures for the models
-
-    // TODO Slightly different blender UI?
-    // TODO JEI/REI support
-
     public static final String MOD_ID = "forbiddensmoothies";
-    public static final Logger logger = LogManager.getLogger(MOD_ID);
 
     public static void initialize() {
         ForbiddenSmoothiesConfig.initialize();
--- shared/src/main/java/net/blay09/mods/forbiddensmoothies/api/ForbiddenSmoothiesAPI.java
+++ shared/src/main/java/net/blay09/mods/forbiddensmoothies/api/ForbiddenSmoothiesAPI.java
@@ -1,19 +1,17 @@
 package net.blay09.mods.forbiddensmoothies.api;
 
-import net.blay09.mods.forbiddensmoothies.InternalMethodsImpl;
-
 import java.lang.reflect.InvocationTargetException;
 
 public class ForbiddenSmoothiesAPI {
 
-    private static final InternalMethodsImpl internalMethods = loadInternalMethods();
+    private static final InternalMethods internalMethods = loadInternalMethods();
 
-    private static InternalMethodsImpl loadInternalMethods() {
+    private static InternalMethods loadInternalMethods() {
         try {
-            return (InternalMethodsImpl) Class.forName("net.blay09.mods.forbiddensmoothies.InternalMethodsImpl").getConstructor().newInstance();
+            return (InternalMethods) Class.forName("net.blay09.mods.forbiddensmoothies.InternalMethodsImpl").getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
                  ClassNotFoundException e) {
-            throw new RuntimeException("Failed to load Crafting for Blockheads API", e);
+            throw new RuntimeException("Failed to load Forbidden Smoothies API", e);
         }
     }
 }
